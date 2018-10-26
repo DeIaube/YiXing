e1825737cf02eb4b131c0332692122ded887d376
Dawn <lu923418335@gmail.com>
2018-10-26 13:10:59 +0800

    增加常量类

---
+++ app/src/main/java/yixing/dawn/zju/edu/yixing/constant/Constant.java
@@ -0,0 +1,11 @@
+package yixing.dawn.zju.edu.yixing.constant;
+
+public interface Constant {
+
+    // isDebug
+    boolean isDebug = true;
+    // SP相关
+    String LOGIN_ACCOUNT = "login_account";
+    String LOGIN_PASSWORD = "login_password";
+
+}
