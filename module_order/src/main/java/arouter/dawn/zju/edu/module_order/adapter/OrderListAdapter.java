2cbbf984cd1e5df038c0fd375330a13da9819921
Dawn <lu923418335@gmail.com>
2018-10-28 22:39:09 +0800

    完善订单列表按钮显示逻辑

--- module_order/src/main/java/arouter/dawn/zju/edu/module_order/adapter/OrderListAdapter.java
+++ module_order/src/main/java/arouter/dawn/zju/edu/module_order/adapter/OrderListAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -15,6 +16,7 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import arouter.dawn.zju.edu.module_order.R;
+import arouter.dawn.zju.edu.module_order.config.Constants;
 import baselib.bean.OrderBean;
 
 public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.OrderListHolder> {
@@ -38,12 +40,27 @@ public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.Orde
     @Override
     public void onBindViewHolder(OrderListHolder holder, int position) {
         OrderBean order = mOrders.get(position);
+        initViewByType(holder, order.getType());
         holder.createTimeTv.setText(order.getCreate_time());
         holder.payNumberTv.setText(String.format("￥ %d", order.getPay()));
         holder.realPayNumberTv.setText(String.format("￥ %d", order.getPay()));
         holder.goodsTitleTv.setText(order.getGoods_title());
         holder.typeTv.setText(order.getType());
         Picasso.with(mContext).load(order.getGoods_preview()).into(holder.goodsPreviewIv);
+    }
+
+    private void initViewByType(OrderListHolder holder, String type) {
+        if (type.equals(Constants.ORDER_TYPE_PAYMENT)) {
+            // 待付款 隐藏申请售后
+            holder.customerServiceBtn.setVisibility(View.GONE);
+        } else if (type.equals(Constants.ORDER_TYPE_CANCEL)) {
+            // 已取消 隐藏 申请售后 取消订单 立即付款
+            holder.buttonLayoutLv.setVisibility(View.GONE);
+        } else if (type.equals(Constants.ORDER_TYPE_COMPLETE)) {
+            // 已完成 隐藏 取消订单 立即付款
+            holder.cancelOrderBtn.setVisibility(View.GONE);
+            holder.payOrderBtn.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -61,6 +78,8 @@ public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.Orde
         TextView realPayNumberTv;
         Button cancelOrderBtn;
         Button payOrderBtn;
+        Button customerServiceBtn;
+        LinearLayout buttonLayoutLv;
 
         OrderListHolder(View itemView) {
             super(itemView);
@@ -72,6 +91,8 @@ public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.Orde
             realPayNumberTv = itemView.findViewById(R.id.real_pay_number);
             cancelOrderBtn = itemView.findViewById(R.id.cancel_order);
             payOrderBtn = itemView.findViewById(R.id.pay_order);
+            customerServiceBtn = itemView.findViewById(R.id.customer_service);
+            buttonLayoutLv = itemView.findViewById(R.id.button_layout);
         }
     }
 }
