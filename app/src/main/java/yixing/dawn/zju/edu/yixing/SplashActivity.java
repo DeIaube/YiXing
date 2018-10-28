92567533302ec3fafc3c75698c342978dbe9d3af
Dawn <lu923418335@gmail.com>
2018-10-28 09:42:30 +0800

    增加RxPermission进行动态权限申请

--- app/src/main/java/yixing/dawn/zju/edu/yixing/SplashActivity.java
+++ app/src/main/java/yixing/dawn/zju/edu/yixing/SplashActivity.java
@@ -1,17 +1,44 @@
 package yixing.dawn.zju.edu.yixing;
 
-import com.alibaba.android.arouter.launcher.ARouter;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.util.Log;
 
-import arouter.dawn.zju.edu.lib_map.LocationManager;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
 import baselib.base.BaseActivity;
 import baselib.config.Constant;
+import io.reactivex.functions.Consumer;
 
 public class SplashActivity extends BaseActivity {
 
+    static final String TAG = "SplashActivity";
+
+    @SuppressLint("CheckResult")
     @Override
     protected void initView() {
-        ARouter.getInstance().build(Constant.AROUTER_LOGIN).navigation();
-        LocationManager.getLocation(this);
+        requestPermission();
+    }
+
+    /**
+     * 动态获取权限
+     */
+    @SuppressLint("CheckResult")
+    private void requestPermission() {
+        new RxPermissions(SplashActivity.this).request(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) throws Exception {
+                        if (aBoolean) {
+                            //所有权限都开启aBoolean才为true，否则为false
+                            Log.i(TAG, "权限已开启");
+                            ARouter.getInstance().build(Constant.AROUTER_LOGIN).navigation();
+                        } else {
+                            Log.i(TAG, "权限被拒绝，请在设置里面开启相应权限，若无相应权限会影响使用");
+                        }
+                    }
+                });
     }
 
     @Override
