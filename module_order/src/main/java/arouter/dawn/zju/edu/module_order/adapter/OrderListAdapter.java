97532f38da94da1c06fc89e1a697c8e67aac2684
Dawn <lu923418335@gmail.com>
2018-10-28 21:23:52 +0800

    更改图片加载框架

--- module_order/src/main/java/arouter/dawn/zju/edu/module_order/adapter/OrderListAdapter.java
+++ module_order/src/main/java/arouter/dawn/zju/edu/module_order/adapter/OrderListAdapter.java
@@ -1,10 +1,16 @@
 package arouter.dawn.zju.edu.module_order.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -28,9 +34,16 @@ public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.Orde
         return new OrderListHolder(rootView);
     }
 
+    @SuppressLint("DefaultLocale")
     @Override
     public void onBindViewHolder(OrderListHolder holder, int position) {
-
+        OrderBean order = mOrders.get(position);
+        holder.createTimeTv.setText(order.getCreate_time());
+        holder.payNumberTv.setText(String.format("￥ %d", order.getPay()));
+        holder.realPayNumberTv.setText(String.format("￥ %d", order.getPay()));
+        holder.goodsTitleTv.setText(order.getGoods_title());
+        holder.typeTv.setText(order.getType());
+        Picasso.with(mContext).load(order.getGoods_preview()).into(holder.goodsPreviewIv);
     }
 
     @Override
@@ -40,8 +53,25 @@ public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.Orde
 
     static class OrderListHolder extends RecyclerView.ViewHolder{
 
+        TextView createTimeTv;
+        TextView typeTv;
+        ImageView goodsPreviewIv;
+        TextView goodsTitleTv;
+        TextView payNumberTv;
+        TextView realPayNumberTv;
+        Button cancelOrderBtn;
+        Button payOrderBtn;
+
         OrderListHolder(View itemView) {
             super(itemView);
+            createTimeTv = itemView.findViewById(R.id.order_create_time);
+            typeTv = itemView.findViewById(R.id.order_type);
+            goodsPreviewIv = itemView.findViewById(R.id.goods_preview);
+            goodsTitleTv = itemView.findViewById(R.id.goods_title);
+            payNumberTv = itemView.findViewById(R.id.pay_number);
+            realPayNumberTv = itemView.findViewById(R.id.real_pay_number);
+            cancelOrderBtn = itemView.findViewById(R.id.cancel_order);
+            payOrderBtn = itemView.findViewById(R.id.pay_order);
         }
     }
 }
--- module_order/src/main/java/arouter/dawn/zju/edu/module_order/ui/order_list/OrderListFragment.java
+++ module_order/src/main/java/arouter/dawn/zju/edu/module_order/ui/order_list/OrderListFragment.java
@@ -30,10 +30,23 @@ public class OrderListFragment extends BaseFragment {
     protected void initView(View view) {
         recyclerView = view.findViewById(R.id.order_list_recycler_view);
         List<OrderBean> orders = new ArrayList<>();
-        orders.add(new OrderBean());
-        orders.add(new OrderBean());
-        orders.add(new OrderBean());
-        orders.add(new OrderBean());
+
+        OrderBean orderBean = new OrderBean();
+        orderBean.setCreate_time("2018-10-28");
+        orderBean.setGoods_title("守望屁股");
+        orderBean.setPay(128);
+        orderBean.setType("待付款");
+        orderBean.setGoods_preview("https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png");
+        orders.add(orderBean);
+
+        OrderBean orderBean2 = new OrderBean();
+        orderBean2.setCreate_time("2018-10-28");
+        orderBean2.setGoods_title("小胡子养成计划");
+        orderBean2.setType("已经完成");
+        orderBean2.setPay(541);
+        orderBean2.setGoods_preview("https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1378027402,433619994&fm=26&gp=0.jpg");
+        orders.add(orderBean2);
+
         OrderListAdapter adapter = new OrderListAdapter(getContext(), orders);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
