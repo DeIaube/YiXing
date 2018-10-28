33ed7901fdd80565bc4c4ff7e8b9a3c75c6755a5
Dawn <lu923418335@gmail.com>
2018-10-28 22:28:27 +0800

    订单模块新增常量类

---
+++ module_order/src/main/java/arouter/dawn/zju/edu/module_order/config/Constant.java
@@ -0,0 +1,9 @@
+package arouter.dawn.zju.edu.module_order.config;
+
+public class Constant {
+
+    String ORDER_TYPE_ALL = "全部";
+    String ORDER_TYPE_PAYMENT = "待付款";
+    String ORDER_TYPE_CANCEL = "已取消";
+    String ORDER_TYPE_COMPLETE = "已完成";
+}
