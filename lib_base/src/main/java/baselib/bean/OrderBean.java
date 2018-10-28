279e8c2aa992783a5136cf21defe6cbd1cb53861
Dawn <lu923418335@gmail.com>
2018-10-28 20:17:00 +0800

    更改OrderBean数据结构

--- lib_base/src/main/java/baselib/bean/OrderBean.java
+++ lib_base/src/main/java/baselib/bean/OrderBean.java
@@ -2,16 +2,16 @@ package baselib.bean;
 
 public class OrderBean {
 
-
-
     /**
+     *
      * {
      *     "id": "123123123123123123",
      *     "account": "15064700768",
      *     "goods_title": "我只是一个小测试",
      *     "goods_preview": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540730977404&di=4e75b92d42235e5896c2c9cf97bc47a2&imgtype=0&src=http%3A%2F%2Fwx2.sinaimg.cn%2Forj360%2Fa8d43f7egy1fhobz9zkr3j20e80e8dja.jpg",
      *     "create_time": "2018-10-28",
-     *     "pay_number": 256
+     *     "pay": 256,
+     *     "type": "待付款"
      * }
      *
      * id : 123123123123123123
@@ -19,7 +19,8 @@ public class OrderBean {
      * goods_title : 我只是一个小测试
      * goods_preview : https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1540730977404&di=4e75b92d42235e5896c2c9cf97bc47a2&imgtype=0&src=http%3A%2F%2Fwx2.sinaimg.cn%2Forj360%2Fa8d43f7egy1fhobz9zkr3j20e80e8dja.jpg
      * create_time : 2018-10-28
-     * pay_number : 256
+     * pay : 256
+     * type : 待付款
      */
 
     private String id;
@@ -27,7 +28,8 @@ public class OrderBean {
     private String goods_title;
     private String goods_preview;
     private String create_time;
-    private int pay_number;
+    private int pay;
+    private String type;
 
     public String getId() {
         return id;
@@ -69,11 +71,19 @@ public class OrderBean {
         this.create_time = create_time;
     }
 
-    public int getPay_number() {
-        return pay_number;
+    public int getPay() {
+        return pay;
     }
 
-    public void setPay_number(int pay_number) {
-        this.pay_number = pay_number;
+    public void setPay(int pay) {
+        this.pay = pay;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
