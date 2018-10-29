d237d4e064c98639b87dba0c3da57de7d22b404b
Dawn <lu923418335@gmail.com>
2018-10-30 01:25:32 +0800

    新增商品列表适配器

---
+++ module_nearby/src/main/java/arouter/dawn/zju/edu/module_nearby/adapter/GoodsListAdapter.java
@@ -0,0 +1,68 @@
+package arouter.dawn.zju.edu.module_nearby.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import arouter.dawn.zju.edu.lib_net.bean.GoodsBean;
+import arouter.dawn.zju.edu.module_nearby.R;
+
+public class GoodsListAdapter extends RecyclerView.Adapter<GoodsListAdapter.GoodsListHolder> {
+
+    List<GoodsBean> mGoodsList;
+    Context mContext;
+
+    public GoodsListAdapter(List<GoodsBean> goodsList, Context context) {
+        this.mGoodsList = goodsList;
+        this.mContext = context;
+    }
+
+    @Override
+    public GoodsListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_goods, parent, false);
+        return new GoodsListHolder(rootView);
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    public void onBindViewHolder(GoodsListHolder holder, int position) {
+        GoodsBean goods = mGoodsList.get(position);
+        holder.titleTv.setText(goods.getTitle());
+        holder.locationTv.setText(String.format("%s %s", goods.getCity(), goods.getRegion()));
+        holder.priceTv.setText(String.format("￥%d", goods.getPrice()));
+        Picasso.with(mContext).load(goods.getPreview()).into(holder.preIv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mGoodsList.size();
+    }
+
+    class GoodsListHolder extends RecyclerView.ViewHolder {
+
+        ImageView preIv;
+        TextView titleTv;
+        TextView locationTv;
+        TextView priceTv;
+        TextView praiseTv;
+
+        public GoodsListHolder(View itemView) {
+            super(itemView);
+            preIv = itemView.findViewById(R.id.goods_pre);
+            titleTv = itemView.findViewById(R.id.goods_title);
+            locationTv = itemView.findViewById(R.id.goods_location);
+            priceTv = itemView.findViewById(R.id.goods_price);
+            praiseTv = itemView.findViewById(R.id.goods_praise);
+
+        }
+    }
+}
