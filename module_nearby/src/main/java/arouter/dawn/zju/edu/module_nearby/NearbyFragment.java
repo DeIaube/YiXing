4f138031f0dd1bb25ab96b0ec0b626ebcb4e0056
Dawn <lu923418335@gmail.com>
2018-10-29 21:27:05 +0800

    增加选择城市页面

--- module_nearby/src/main/java/arouter/dawn/zju/edu/module_nearby/NearbyFragment.java
+++ module_nearby/src/main/java/arouter/dawn/zju/edu/module_nearby/NearbyFragment.java
@@ -1,27 +1,22 @@
 package arouter.dawn.zju.edu.module_nearby;
 
 
-import android.app.ActionBar;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.content.Intent;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
+import com.zaaach.citypicker.CityPickerActivity;
 
-import java.util.Objects;
-
-import baselib.base.BaseActivity;
 import baselib.base.BaseFragment;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class NearbyFragment extends BaseFragment {
 
     TextView searchTv;
-    ImageView locationTv;
+    TextView loacationTv;
+
+    private static final int REQUEST_CODE_PICK_CITY = 0;
 
     @Override
     protected int getLayoutId() {
@@ -36,6 +31,25 @@ public class NearbyFragment extends BaseFragment {
     @Override
     protected void initView(View view) {
         searchTv = view.findViewById(R.id.near_search);
-        locationTv = view.findViewById(R.id.near_location);
+        loacationTv = view.findViewById(R.id.near_location);
+        loacationTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(getContext(), CityPickerActivity.class),
+                        REQUEST_CODE_PICK_CITY);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICK_CITY && resultCode == RESULT_OK){
+            if (data != null){
+                String city = data.getStringExtra(CityPickerActivity.KEY_PICKED_CITY);
+                showMessage("当前选择：" + city);
+            }
+        }
+
     }
 }
