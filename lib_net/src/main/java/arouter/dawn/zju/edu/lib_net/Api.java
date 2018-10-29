67c0f8999b723ba16f39b238aab2e38f86326f22
Dawn <lu923418335@gmail.com>
2018-10-30 01:29:42 +0800

    新增搜索api

--- lib_net/src/main/java/arouter/dawn/zju/edu/lib_net/Api.java
+++ lib_net/src/main/java/arouter/dawn/zju/edu/lib_net/Api.java
@@ -2,12 +2,16 @@ package arouter.dawn.zju.edu.lib_net;
 
 
 import arouter.dawn.zju.edu.lib_net.bean.network.ObtainOrderRespense;
+import arouter.dawn.zju.edu.lib_net.bean.network.SearchGoodsRespense;
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface Api {
 
-    @GET("obtain_order")
+    @GET(Constants.OBTAIN_ORDER)
     Observable<ObtainOrderRespense> obtainOrder();
+
+    @GET(Constants.SEARCH)
+    Observable<SearchGoodsRespense> search();
 
 }
--- lib_net/src/main/java/arouter/dawn/zju/edu/lib_net/Constants.java
+++ lib_net/src/main/java/arouter/dawn/zju/edu/lib_net/Constants.java
@@ -5,5 +5,6 @@ public interface Constants {
     String BASE_URL = "http://www.wanandroid.com/tools/mockapi/12173/";
 
     String OBTAIN_ORDER = "obtain_order";
+    String SEARCH = "search";
 
 }
