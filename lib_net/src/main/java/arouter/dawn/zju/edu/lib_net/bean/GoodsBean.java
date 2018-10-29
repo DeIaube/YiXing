eb3239c8d0d3aeb17433ae00e3e14799a5cb8522
Dawn <lu923418335@gmail.com>
2018-10-30 00:06:46 +0800

    增加搜索响应实体类

---
+++ lib_net/src/main/java/arouter/dawn/zju/edu/lib_net/bean/GoodsBean.java
@@ -0,0 +1,87 @@
+package arouter.dawn.zju.edu.lib_net.bean;
+
+public class GoodsBean {
+    /**
+     * id : 123123123123123123
+     * title : 小米8 青春版
+     * explain : 全面屏智能拍照游戏手机学生机商务机 全网通 官方旗舰正品
+     * preview : http://attach.bbs.miui.com/forum/201805/31/181909by8vfl4gxglq8z9h.jpg
+     * create_time : 2018-10-28
+     * city : 宁波
+     * type : 数码产品
+     * price : 2399
+     */
+
+    private String id;
+    private String title;
+    private String explain;
+    private String preview;
+    private String create_time;
+    private String city;
+    private String type;
+    private int price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExplain() {
+        return explain;
+    }
+
+    public void setExplain(String explain) {
+        this.explain = explain;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
