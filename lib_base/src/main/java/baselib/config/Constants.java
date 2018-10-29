04646a8bff81cfcf75b086f44a589c3eefe2730b
Dawn <lu923418335@gmail.com>
2018-10-29 22:26:54 +0800

    更改ARouter path

--- lib_base/src/main/java/baselib/config/Constants.java
+++ lib_base/src/main/java/baselib/config/Constants.java
@@ -6,10 +6,11 @@ public interface Constants {
     boolean isDebug = true;
 
     // ARouter
-    String AROUTER_MAIN = "/app/main";
-    String AROUTER_LOGIN = "/account/login";
-    String AROUTER_REGISTER = "/account/register";
-    String AROUTER_RESET_PASSWORD = "/account/reset_password";
+    String AROUTER_MAIN = "/app/main_main";
+    String AROUTER_LOGIN = "/account/account_login";
+    String AROUTER_REGISTER = "/account/account_register";
+    String AROUTER_RESET_PASSWORD = "/account/account_reset_password";
+    String AROUTER_SEARCH = "/account/nearby_search";
 
     String PHONE_NUMBER = "phoneNumber";
 
