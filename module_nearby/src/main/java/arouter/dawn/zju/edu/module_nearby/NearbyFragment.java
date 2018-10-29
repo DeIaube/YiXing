485ba088d70756e2749ed06d4ab570bce882f2e5
Dawn <lu923418335@gmail.com>
2018-10-29 20:23:11 +0800

    完善附近页面逻辑

--- module_nearby/src/main/java/arouter/dawn/zju/edu/module_nearby/NearbyFragment.java
+++ module_nearby/src/main/java/arouter/dawn/zju/edu/module_nearby/NearbyFragment.java
@@ -1,13 +1,27 @@
 package arouter.dawn.zju.edu.module_nearby;
 
 
+import android.app.ActionBar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Objects;
+
+import baselib.base.BaseActivity;
 import baselib.base.BaseFragment;
 
 
 public class NearbyFragment extends BaseFragment {
+
+    TextView searchTv;
+    ImageView locationTv;
 
     @Override
     protected int getLayoutId() {
@@ -21,6 +35,7 @@ public class NearbyFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
-
+        searchTv = view.findViewById(R.id.near_search);
+        locationTv = view.findViewById(R.id.near_location);
     }
 }
