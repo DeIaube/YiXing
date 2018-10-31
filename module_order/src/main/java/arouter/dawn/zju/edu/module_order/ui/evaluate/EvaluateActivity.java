d4e92f62b8644fe4f90c0fac84e9afb60aed395f
Dawn <lu923418335@gmail.com>
2018-10-31 21:55:38 +0800

    完善商品评价界面

--- module_order/src/main/java/arouter/dawn/zju/edu/module_order/ui/evaluate/EvaluateActivity.java
+++ module_order/src/main/java/arouter/dawn/zju/edu/module_order/ui/evaluate/EvaluateActivity.java
@@ -1,4 +1,8 @@
-package arouter.dawn.zju.edu.module_order.ui;
+package arouter.dawn.zju.edu.module_order.ui.evaluate;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
@@ -7,11 +11,30 @@ import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_ORDER_EVALUATE)
-public class EvaluateActivity extends BaseActivity {
+public class EvaluateActivity extends BaseActivity implements View.OnClickListener {
+
+    ImageView frownIv;
+    ImageView mehIv;
+    ImageView smileIv;
+    TextView smileTv;
+    TextView mehTv;
+    TextView frownTv;
 
     @Override
     protected void initView() {
+        frownIv = findViewById(R.id.frown_image);
+        mehIv = findViewById(R.id.meh_image);
+        smileIv = findViewById(R.id.smile_image);
+        smileTv = findViewById(R.id.smile_text);
+        mehTv = findViewById(R.id.meh_text);
+        frownTv = findViewById(R.id.frown_text);
 
+        frownIv.setOnClickListener(this);
+        mehIv.setOnClickListener(this);
+        smileIv.setOnClickListener(this);
+        smileTv.setOnClickListener(this);
+        mehTv.setOnClickListener(this);
+        frownTv.setOnClickListener(this);
     }
 
     @Override
@@ -24,4 +47,20 @@ public class EvaluateActivity extends BaseActivity {
 
     }
 
+    @Override
+    protected boolean showHomeAsUp() {
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.smile_text || id == R.id.smile_image) {
+
+        } else if (id == R.id.meh_text || id == R.id.meh_image) {
+
+        } else if (id == R.id.frown_text || id == R.id.frown_image) {
+
+        }
+    }
 }
