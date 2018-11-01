bb25428897e0a099c3bab457d845bc8d1609482e
Dawn <lu923418335@gmail.com>
2018-11-01 12:36:19 +0800

    新增MineFragment

---
+++ module_mine/src/main/java/arouter/dawn/zju/edu/module_mine/MineFragment.java
@@ -0,0 +1,23 @@
+package arouter.dawn.zju.edu.module_mine;
+
+import android.view.View;
+
+import baselib.base.BaseFragment;
+
+public class MineFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_mine;
+    }
+
+    @Override
+    protected void bindPresenter() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
