dc76ed1545c677e28e9bda22b2147b9caf35a400
Dawn <lu923418335@gmail.com>
2018-11-02 17:29:54 +0800

    完善设置页面逻辑

--- module_setting/src/main/java/arouter/dawn/zju/edu/module_mine/ui/setting/SettingFragment.java
+++ module_setting/src/main/java/arouter/dawn/zju/edu/module_mine/ui/setting/SettingFragment.java
@@ -1,7 +1,12 @@
 package arouter.dawn.zju.edu.module_mine.ui.setting;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
+
 
 import arouter.dawn.zju.edu.module_mine.R;
 
@@ -10,5 +15,20 @@ public class SettingFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.preference_setting);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceScreen ps = (PreferenceScreen) findPreference("clear_cache");
+        ps.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @SuppressLint("CheckResult")
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(getContext(), "缓存已清除", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
     }
 }
