b9c39cfd970f79cae3b65172057de1470e48cd3d
Dawn <lu923418335@gmail.com>
2018-11-03 01:04:25 +0800

    完成购物车item页面布局

--- module_goods/src/main/java/arouter/dawn/zju/edu/module_goods/adapter/CartGoodsListAdapter.java
+++ module_goods/src/main/java/arouter/dawn/zju/edu/module_goods/adapter/CartGoodsListAdapter.java
@@ -1,12 +1,19 @@
 package arouter.dawn.zju.edu.module_goods.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import arouter.dawn.zju.edu.module_nearby.R;
 import baselib.bean.Goods;
 
 public class CartGoodsListAdapter extends RecyclerView.Adapter<CartGoodsListAdapter.CartGoodsHolder> {
@@ -26,12 +33,16 @@ public class CartGoodsListAdapter extends RecyclerView.Adapter<CartGoodsListAdap
 
     @Override
     public CartGoodsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View rootView = LayoutInflater.from(context).inflate(R.layout.item_cart_goods, parent, false);
+        return new CartGoodsHolder(rootView);
     }
 
     @Override
     public void onBindViewHolder(CartGoodsHolder holder, int position) {
-
+//        Goods goods = goodsList.get(position);
+//        Picasso.with(context).load(goods.getPreview()).into(holder.previewIv);
+//        holder.titleTv.setText(goods.getTitle());
+//        holder.priveTv.setText(String.format("￥%d", goods.getPrice()));
     }
 
     @Override
@@ -41,8 +52,17 @@ public class CartGoodsListAdapter extends RecyclerView.Adapter<CartGoodsListAdap
 
     class CartGoodsHolder extends RecyclerView.ViewHolder{
 
+        AppCompatCheckBox selectCb;
+        ImageView previewIv;
+        TextView titleTv;
+        TextView priveTv;
+
         public CartGoodsHolder(View itemView) {
             super(itemView);
+//            selectCb = itemView.findViewById(R.id.select);
+//            previewIv = itemView.findViewById(R.id.preview);
+//            titleTv = itemView.findViewById(R.id.title);
+//            priveTv = itemView.findViewById(R.id.price);
         }
     }
 }
