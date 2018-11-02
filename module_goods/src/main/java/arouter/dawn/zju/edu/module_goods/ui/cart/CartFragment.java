1ba42c103280259169a9611a5a27d0e4bd112bb6
Dawn <lu923418335@gmail.com>
2018-11-02 22:56:14 +0800

    自定义toolbar

--- module_goods/src/main/java/arouter/dawn/zju/edu/module_goods/ui/cart/CartFragment.java
+++ module_goods/src/main/java/arouter/dawn/zju/edu/module_goods/ui/cart/CartFragment.java
@@ -1,6 +1,8 @@
 package arouter.dawn.zju.edu.module_goods.ui.cart;
 
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
@@ -11,6 +13,8 @@ import baselib.config.Constants;
 @Route(path = Constants.AROUTER_GOODS_CART)
 public class CartFragment extends BaseFragment<CartContract.Presenter> implements CartContract.View {
 
+    TextView toolTextTv;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_cart;
@@ -18,12 +22,14 @@ public class CartFragment extends BaseFragment<CartContract.Presenter> implement
 
     @Override
     protected void bindPresenter() {
-
+        mPresenter = new CartPresenter();
     }
 
     @Override
     protected void initView(View view) {
+        toolTextTv = view.findViewById(R.id.tool_text);
 
+        toolTextTv.setText("购物车");
     }
 
 }
