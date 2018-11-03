5bca46909e652b10766bfb5b12eb1b19d5c72056
Dawn <lu923418335@gmail.com>
2018-11-03 20:04:32 +0800

    增加通知数据类

--- lib_db/src/main/java/arouter/dawn/zju/edu/lib_db/bean/Notice.java
+++ lib_db/src/main/java/arouter/dawn/zju/edu/lib_db/bean/Notice.java
@@ -1,4 +1,53 @@
 package arouter.dawn.zju.edu.lib_db.bean;
 
-public class Notice {
+import io.realm.RealmObject;
+
+public class Notice extends RealmObject {
+
+    private String title;
+    private String content;
+    private String time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Notice{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+
+    public Notice(String title, String content, String time) {
+        this.title = title;
+        this.content = content;
+        this.time = time;
+    }
+
+    public Notice() {
+
+    }
 }
