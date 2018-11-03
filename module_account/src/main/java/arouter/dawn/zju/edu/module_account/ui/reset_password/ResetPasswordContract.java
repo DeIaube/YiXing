694ba47c463949aa610c2f98ed79eab651a92ec4
Dawn <lu923418335@gmail.com>
2018-11-04 01:37:25 +0800

    完善重置密码逻辑

---
+++ module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/reset_password/ResetPasswordContract.java
@@ -0,0 +1,15 @@
+package arouter.dawn.zju.edu.module_account.ui.set_password;
+
+import baselib.base.BaseContract;
+
+public interface SetPasswordContract {
+
+    interface View extends BaseContract.BaseView {
+        void setPasswordSuccess();
+    }
+
+    interface Presenter extends BaseContract.BasePresenter<View> {
+        void resetPassword(final String phoneNumber, final String password, final String repassword);
+    }
+
+}
