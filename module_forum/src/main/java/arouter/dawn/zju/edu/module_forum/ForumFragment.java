fe240be34ce55ef4ca64f54eb86c338dd507f1a6
Dawn <lu923418335@gmail.com>
2018-11-04 22:40:19 +0800

    新增论坛页面

---
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ForumFragment.java
@@ -0,0 +1,23 @@
+package arouter.dawn.zju.edu.module_forum;
+
+
+import android.view.View;
+
+import baselib.base.BaseFragment;
+
+public class ForumFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_forum;
+    }
+
+    @Override
+    protected void bindPresenter() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
