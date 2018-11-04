d2af70226d9499c911f244cdf843d95a84a2a22f
Dawn <lu923418335@gmail.com>
2018-11-04 15:26:20 +0800

    完善部分逻辑

--- module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/modify_pickname/ModifyPicknameActivity.java
+++ module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/modify_pickname/ModifyPicknameActivity.java
@@ -1,6 +1,10 @@
 package arouter.dawn.zju.edu.module_account.ui.modify_pickname;
 
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import arouter.dawn.zju.edu.module_account.R;
@@ -8,11 +12,16 @@ import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_ACCOUNT_MODIFY_PICKNAME)
-public class ModifyPicknameActivity extends BaseActivity<ModifyPicknameContract.Presenter> implements ModifyPicknameContract.View {
+public class ModifyPicknameActivity extends BaseActivity<ModifyPicknameContract.Presenter> implements ModifyPicknameContract.View, View.OnClickListener {
 
+    EditText modifyPicknameEt;
+    Button confirmModifyBtn;
+    
     @Override
     protected void initView() {
-
+        modifyPicknameEt = findViewById(R.id.modify_pickname_pickname);
+        confirmModifyBtn = findViewById(R.id.modify_pickname_confirm_modify);
+        confirmModifyBtn.setOnClickListener(this);
     }
 
     @Override
@@ -28,5 +37,13 @@ public class ModifyPicknameActivity extends BaseActivity<ModifyPicknameContract.
     @Override
     protected boolean showHomeAsUp() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.modify_pickname_confirm_modify) {
+
+        }
     }
 }
