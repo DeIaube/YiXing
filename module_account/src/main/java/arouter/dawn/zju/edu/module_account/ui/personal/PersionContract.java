cc273c990165a275f01438bf4f7b0c031d1c70f5
Dawn <lu923418335@gmail.com>
2018-11-04 14:21:49 +0800

    完善个人资料页面

---
+++ module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/personal/PersionContract.java
@@ -0,0 +1,15 @@
+package arouter.dawn.zju.edu.module_account.ui.personal;
+
+import baselib.base.BaseContract;
+
+public interface PersionContract {
+
+    interface View extends BaseContract.BaseView {
+
+    }
+
+    interface Presenter extends BaseContract.BasePresenter<View> {
+
+    }
+
+}
---
+++ module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/personal/PersionPresenter.java
@@ -0,0 +1,9 @@
+package arouter.dawn.zju.edu.module_account.ui.personal;
+
+import baselib.base.BasePresenter;
+
+public class PersionPresenter extends BasePresenter<PersionContract.View> implements PersionContract.Presenter {
+
+    private static final String TAG = "PersionPresenter";
+
+}
