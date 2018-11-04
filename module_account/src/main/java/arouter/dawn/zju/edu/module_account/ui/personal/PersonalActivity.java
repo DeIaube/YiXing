cc1404eee0892f59a81053e84c6f8175de8e54d3
Dawn <lu923418335@gmail.com>
2018-11-04 14:46:01 +0800

    完善个人资料部分逻辑

--- module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/personal/PersonalActivity.java
+++ module_account/src/main/java/arouter/dawn/zju/edu/module_account/ui/personal/PersonalActivity.java
@@ -1,17 +1,35 @@
 package arouter.dawn.zju.edu.module_account.ui.personal;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
+import arouter.dawn.zju.edu.lib_net.bean.User;
 import arouter.dawn.zju.edu.module_account.R;
 import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_ACCOUNT_PERSONAL)
-public class PersonalActivity extends BaseActivity {
+public class PersonalActivity extends BaseActivity implements View.OnClickListener {
+
+    TextView personalUsernameTv;
+    TextView personalPhoneNumberTv;
+    ImageView personalPortraitIv;
 
     @Override
     protected void initView() {
+        personalUsernameTv = findViewById(R.id.personal_username);
+        personalPhoneNumberTv = findViewById(R.id.personal_bind_phone_number);
+        personalPortraitIv = findViewById(R.id.personal_portrait);
 
+        findViewById(R.id.check_portrait).setOnClickListener(this);
+        findViewById(R.id.check_username).setOnClickListener(this);
+
+        User user = User.getCurrentUser(User.class);
+        personalUsernameTv.setText(user.getName());
+        personalPhoneNumberTv.setText(user.getMobilePhoneNumber());
     }
 
     @Override
@@ -27,5 +45,15 @@ public class PersonalActivity extends BaseActivity {
     @Override
     protected boolean showHomeAsUp() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.check_portrait) {
+            // 选取头像
+        } else if (id == R.id.check_username) {
+            // 点击用户昵称
+        }
     }
 }
