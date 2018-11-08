1d8fa95131d0a77477d744d3d823cdffedaf2898
Dawn <lu923418335@gmail.com>
2018-11-08 21:55:14 +0800

    增加常量

---
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/config/Constants.java
@@ -0,0 +1,12 @@
+package arouter.dawn.zju.edu.module_forum.config;
+
+public interface Constants {
+    String TYPE_HOME = "首页";
+    String TYPE_HUMANITY= "人文";
+    String TYPE_INTELLECTUALITY = "知性";
+    String TYPE_KEISURE = "休闲";
+    String TYPE_SPORTS = "体育";
+    String TYPE_FINANCE = "财经";
+    String TYPE_FASHION = "时尚";
+    String TYPE_EMOTION = "情感";
+}
