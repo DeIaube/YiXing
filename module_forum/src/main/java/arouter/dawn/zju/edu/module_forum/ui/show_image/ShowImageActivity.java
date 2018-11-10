883c08769df3c30d61ef0de0372458da4272d345
Dawn <lu923418335@gmail.com>
2018-11-10 11:11:10 +0800

    增加查看图片页面

---
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ui/show_image/ShowImageActivity.java
@@ -0,0 +1,23 @@
+package arouter.dawn.zju.edu.module_forum.ui.show_image;
+
+import arouter.dawn.zju.edu.module_forum.R;
+import baselib.base.BaseActivity;
+
+public class ShowImageActivity extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_show_image;
+    }
+
+    @Override
+    protected void bindPresenter() {
+
+    }
+
+}
