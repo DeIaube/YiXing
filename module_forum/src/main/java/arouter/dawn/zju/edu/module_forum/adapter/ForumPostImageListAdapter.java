aa11230579436a8c76a6c1a2b45f69dfa559ec9a
Dawn <lu923418335@gmail.com>
2018-11-13 15:30:28 +0800

    完善帖子内容页面

--- module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/adapter/ForumPostImageListAdapter.java
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/adapter/ForumPostImageListAdapter.java
@@ -1,4 +1,70 @@
 package arouter.dawn.zju.edu.module_forum.adapter;
 
-public class ForumPostImageListAdapter {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import arouter.dawn.zju.edu.module_forum.R;
+import baselib.config.Constants;
+
+public class ForumPostImageListAdapter extends RecyclerView.Adapter<ForumPostImageListAdapter.PostImageListHolder> {
+
+    private Context mContext;
+    private ArrayList<String> mImageList;
+
+    public ForumPostImageListAdapter(Context context, List<String> urls) {
+        this.mContext = context;
+        this.mImageList = new ArrayList<>(urls);
+    }
+
+    @NonNull
+    @Override
+    public PostImageListHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View rootView = LayoutInflater.from(mContext).inflate(R.layout.item_forum_post_list_image, viewGroup, false);
+        return new PostImageListHolder(rootView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostImageListHolder holder, int i) {
+        holder.position = i;
+        Picasso.with(mContext).load(mImageList.get(i)).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageList.size();
+    }
+
+    class PostImageListHolder extends RecyclerView.ViewHolder {
+
+        int position;
+        ImageView imageView;
+
+        public PostImageListHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle bundle = new Bundle();
+                    bundle.putStringArrayList(Constants.FORUM_SHOW_IMAGE_LIST, (mImageList));
+                    ARouter.getInstance().build(Constants.AROUTER_FORUM_SHOW_IMAGE).
+                            withBundle(Constants.FORUM_SHOW_IMAGE_BUNDLE, bundle).
+                            withInt(Constants.FORUM_SHOW_IMAGE_POSTION, position).navigation();
+                }
+            });
+        }
+    }
 }
--- module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ui/post/ForumPostActivity.java
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ui/post/ForumPostActivity.java
@@ -1,14 +1,21 @@
 package arouter.dawn.zju.edu.module_forum.ui.post;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.squareup.picasso.Picasso;
 
 import arouter.dawn.zju.edu.lib_net.bean.ForumPost;
 import arouter.dawn.zju.edu.module_forum.R;
+import arouter.dawn.zju.edu.module_forum.adapter.ForumPostImageListAdapter;
 import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
@@ -18,8 +25,31 @@ public class ForumPostActivity extends BaseActivity<ForumPostContract.Presenter>
     @Autowired(name = Constants.FORUM_POST_POST)
     ForumPost post;
 
+    TextView authorNameTv;
+    ImageView authorPortraitIv;
+    TextView postTitleTv;
+    TextView postContentTv;
+    Button followBtn;
+    RecyclerView postImageListRv;
+
+    private ForumPostImageListAdapter mInageListAdapter;
+
     @Override
     protected void initView() {
+        authorNameTv = findViewById(R.id.forum_post_author_name);
+        authorPortraitIv = findViewById(R.id.forum_post_author_portrait);
+        postTitleTv = findViewById(R.id.post_title);
+        postContentTv = findViewById(R.id.forum_post_content);
+        followBtn = findViewById(R.id.forum_post_follow);
+        postImageListRv = findViewById(R.id.forum_post_image_list_view);
+
+        authorNameTv.setText(post.getAuthor().getPickName());
+        Picasso.with(this).load(post.getAuthor().getPortrait()).into(authorPortraitIv);
+        postTitleTv.setText(post.getTitle());
+        postContentTv.setText(post.getContent());
+        mInageListAdapter = new ForumPostImageListAdapter(this, post.getImageList());
+        postImageListRv.setLayoutManager(new LinearLayoutManager(this));
+        postImageListRv.setAdapter(mInageListAdapter);
     }
 
     @Override
@@ -49,7 +79,7 @@ public class ForumPostActivity extends BaseActivity<ForumPostContract.Presenter>
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.post_menu_collection) {
-            
+
         } else if (id == R.id.post_menu_share) {
 
         } else if (id == R.id.post_menu_report) {
