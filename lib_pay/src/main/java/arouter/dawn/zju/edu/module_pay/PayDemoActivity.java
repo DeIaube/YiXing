abdb3590c0a2954e416134f7ae5adbfde754ff40
Dawn <lu923418335@gmail.com>
2018-11-17 12:37:35 +0800

    refactor 优化代码

--- lib_pay/src/main/java/arouter/dawn/zju/edu/module_pay/PayDemoActivity.java
+++ lib_pay/src/main/java/arouter/dawn/zju/edu/module_pay/PayDemoActivity.java
@@ -26,9 +26,6 @@ import arouter.dawn.zju.edu.module_pay.util.OrderInfoUtil2_0;
  *  防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险； 
  */
 public class PayDemoActivity extends Activity {
-	
-	public static final String APPID = "2016080100137923";
-	public static final String RSA2_PRIVATE = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChP753r6lVRpUxQMQ/QUmQ+tWfKxmidDmLH3iVCPaUlx3uQTAM3j6tXQ/K+crFVZ5L/H5iO80gZX7X1G+t0wY/rbuMIDQmxP0Y2IvBeSYicj6M8YXmYp0X+bsWBP0SNQ/hv7cl/VrHJuZcuxmyS8yWmAlcMchrWNqFO1E7bhFpztD+K/Fo/p8unZmWrpz2MOpyitD+WJ1Lf3j94Ncw6aUcv0YoaKAVLteqBnOystsBhzC5F93KeP6LPjtDvxV7h5T9ehaBc7ONdajlYefMAPOqrwePzpCEvRBFGDdPYhmYoV0AkEhhP1h3vsra7oeKC5nrCLp8+UVDOLZRebFsvisJAgMBAAECggEBAKDFjKOKylvS7yfXepPTBYV/AwUbFQkLtuAGtdRqxmfqsBYAeucpo/Z4ZimrmsfcEOOUUXKiS2h0qd6J2fvG/mD5WJhi93x8RhnCtr1ljGcl30Xp/eZyBk2EWkufF7BSpY4g93vyOtN77o0go4NI3ih8WEDLBhg51E6okNqfaM9DoTSGYS7jj0F+CmeZzEO8Jut0gLmEte2n6q9GI83OHZ1tU/fWrdSaj0oNNy+QCYxnKuGhxQPDdniQFEECh/y3McsVRgpSCYUKKW6xlf4ZnR0Yfv0ugPGgMWm8VKMbKj50EK5A1+n6ulLX3bQFTMHOPjtNglElNidAi3/6Kxx1hPECgYEA8s/rdLSjVZgutRlimPx4crFWyRJS75kLL0344jlQ+tZmsHvjzJIfTu9YWmZmMh3x2apOjGTPH9UQuzxJiV0do+Za80b3o/wbWextQ0rEjkMagQXqQbt2W0eo2IMRiyOvpFASS2pJd9RnevghQAHSaCIkK+wITD1pgxv4o4YhZQ0CgYEAqgHDXKnur5FDswdd55jK+g3UYKDgl0ox10BxGUgHYlOPmsIvg4Z5t+OfdgZtwruggC/+s/93WGLiJ2iO90j11gx1B9xB8enrZeJfmXQ3+dvKhyqwCkF7MYVkH/J+icXLXaOHsRzz4NPpvbu60dZjH1t0T1Kbcz99/TtJvx2Wlu0CgYBDMmluFeOx93jNo6XWwuAulYd4A/sx1b/zqnd7W/NeR+GJa+/iGHpTpQdllTspGYJqA6jTT77Gh3em6YtMWZmvVzvKYdR87HNBUp32ZoW4/Tcjh9iJsnCR2GYQ7Uz4nbqpFYPKnTKR2BV/QZYqGZb6PKCYCy1MtB40K5SG5MLvLQKBgE98t40x3umzr8WbIirQ48Z6hNwRHDFgfWE5Pigl513v1+rx9Ck4zNxPzxTt+I/eDjMhPaOHRJPM2BS47G9ua5MDDAz6zNKBHpDqgOxMzIdEXrNIIDsLVsBWr/PH+6YLFwTXGvCSquxp6JXPmo/V/06AAie8ZfZCr3OhLt/uw6QZAoGAdOtBRijHw3F0sSxSN+vs47fmxUSWOnre79aDVIr80x6XTGVNLuQSYvq8WuDLrXEd5LWjUAwaVOxpiDc77OiP79MBws4HNIXOgZ5hUzNvj0N1Uu8uCGujWJbRTEGMRcruoSj1msF8/mAgxXCCdbMKTBPeKsSNIrgpKwNltWOXyAM=";
 
 	private static final int SDK_PAY_FLAG = 1;
 
@@ -56,7 +53,7 @@ public class PayDemoActivity extends Activity {
 				break;
 			}
 			}
-		};
+		}
 	};
 
 	@Override
@@ -86,11 +83,10 @@ public class PayDemoActivity extends Activity {
 		 * 
 		 * orderInfo的获取必须来自服务端；
 		 */
-        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
-		Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(APPID, rsa2);
+		Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap("tutke", "content", 198);
 		String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
 
-		String sign = OrderInfoUtil2_0.getSign(params, RSA2_PRIVATE, rsa2);
+		String sign = OrderInfoUtil2_0.getSign(params, true);
 		final String orderInfo = orderParam + "&" + sign;
 
 		Runnable payRunnable = new Runnable() {
--- lib_pay/src/main/java/arouter/dawn/zju/edu/module_pay/util/OrderInfoUtil2_0.java
+++ lib_pay/src/main/java/arouter/dawn/zju/edu/module_pay/util/OrderInfoUtil2_0.java
@@ -16,7 +16,7 @@ import java.util.Random;
 import arouter.dawn.zju.edu.module_pay.SignUtils;
 
 public class OrderInfoUtil2_0 {
-	
+
 	/**
 	 * 构造授权参数列表
 	 * 
@@ -63,21 +63,20 @@ public class OrderInfoUtil2_0 {
 
 	/**
 	 * 构造支付订单参数列表
-	 * @param app_id
 	 * @return
 	 */
-	public static Map<String, String> buildOrderParamMap(String app_id, boolean rsa2) {
+	public static Map<String, String> buildOrderParamMap(String orderTitle, String orderContent, double orderPrice) {
 		Map<String, String> keyValues = new HashMap<String, String>();
+		keyValues.put("app_id", "2016080100137923");
+		String sss = "{\"timeout_express\":\"30m\",\"product_code\":\"QUICK_MSECURITY_PAY\",\"total_amount\":\"" + orderPrice + "\",\"subject\":\""+orderTitle+"\",\"body\":\""+orderContent+"\",\"out_trade_no\":\"" + getOutTradeNo() +  "\"}";
 
-		keyValues.put("app_id", app_id);
-
-		keyValues.put("biz_content", "{\"timeout_express\":\"30m\",\"product_code\":\"QUICK_MSECURITY_PAY\",\"total_amount\":\"0.01\",\"subject\":\"1\",\"body\":\"我是测试数据\",\"out_trade_no\":\"" + getOutTradeNo() +  "\"}");
+		keyValues.put("biz_content", sss);
 
 		keyValues.put("charset", "utf-8");
 
 		keyValues.put("method", "alipay.trade.app.pay");
 
-		keyValues.put("sign_type", rsa2 ? "RSA2" : "RSA");
+		keyValues.put("sign_type", "RSA2");
 
 		keyValues.put("timestamp", "2016-07-29 16:55:53");
 
@@ -143,7 +142,7 @@ public class OrderInfoUtil2_0 {
 	 * 
 	 * @return
 	 */
-	public static String getSign(Map<String, String> map, String rsaKey, boolean rsa2) {
+	public static String getSign(Map<String, String> map, boolean rsa2) {
 		List<String> keys = new ArrayList<String>(map.keySet());
 		// key排序
 		Collections.sort(keys);
@@ -160,7 +159,7 @@ public class OrderInfoUtil2_0 {
 		String tailValue = map.get(tailKey);
 		authInfo.append(buildKeyValue(tailKey, tailValue, false));
 
-		String oriSign = SignUtils.sign(authInfo.toString(), rsaKey, rsa2);
+		String oriSign = SignUtils.sign(authInfo.toString(), "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChP753r6lVRpUxQMQ/QUmQ+tWfKxmidDmLH3iVCPaUlx3uQTAM3j6tXQ/K+crFVZ5L/H5iO80gZX7X1G+t0wY/rbuMIDQmxP0Y2IvBeSYicj6M8YXmYp0X+bsWBP0SNQ/hv7cl/VrHJuZcuxmyS8yWmAlcMchrWNqFO1E7bhFpztD+K/Fo/p8unZmWrpz2MOpyitD+WJ1Lf3j94Ncw6aUcv0YoaKAVLteqBnOystsBhzC5F93KeP6LPjtDvxV7h5T9ehaBc7ONdajlYefMAPOqrwePzpCEvRBFGDdPYhmYoV0AkEhhP1h3vsra7oeKC5nrCLp8+UVDOLZRebFsvisJAgMBAAECggEBAKDFjKOKylvS7yfXepPTBYV/AwUbFQkLtuAGtdRqxmfqsBYAeucpo/Z4ZimrmsfcEOOUUXKiS2h0qd6J2fvG/mD5WJhi93x8RhnCtr1ljGcl30Xp/eZyBk2EWkufF7BSpY4g93vyOtN77o0go4NI3ih8WEDLBhg51E6okNqfaM9DoTSGYS7jj0F+CmeZzEO8Jut0gLmEte2n6q9GI83OHZ1tU/fWrdSaj0oNNy+QCYxnKuGhxQPDdniQFEECh/y3McsVRgpSCYUKKW6xlf4ZnR0Yfv0ugPGgMWm8VKMbKj50EK5A1+n6ulLX3bQFTMHOPjtNglElNidAi3/6Kxx1hPECgYEA8s/rdLSjVZgutRlimPx4crFWyRJS75kLL0344jlQ+tZmsHvjzJIfTu9YWmZmMh3x2apOjGTPH9UQuzxJiV0do+Za80b3o/wbWextQ0rEjkMagQXqQbt2W0eo2IMRiyOvpFASS2pJd9RnevghQAHSaCIkK+wITD1pgxv4o4YhZQ0CgYEAqgHDXKnur5FDswdd55jK+g3UYKDgl0ox10BxGUgHYlOPmsIvg4Z5t+OfdgZtwruggC/+s/93WGLiJ2iO90j11gx1B9xB8enrZeJfmXQ3+dvKhyqwCkF7MYVkH/J+icXLXaOHsRzz4NPpvbu60dZjH1t0T1Kbcz99/TtJvx2Wlu0CgYBDMmluFeOx93jNo6XWwuAulYd4A/sx1b/zqnd7W/NeR+GJa+/iGHpTpQdllTspGYJqA6jTT77Gh3em6YtMWZmvVzvKYdR87HNBUp32ZoW4/Tcjh9iJsnCR2GYQ7Uz4nbqpFYPKnTKR2BV/QZYqGZb6PKCYCy1MtB40K5SG5MLvLQKBgE98t40x3umzr8WbIirQ48Z6hNwRHDFgfWE5Pigl513v1+rx9Ck4zNxPzxTt+I/eDjMhPaOHRJPM2BS47G9ua5MDDAz6zNKBHpDqgOxMzIdEXrNIIDsLVsBWr/PH+6YLFwTXGvCSquxp6JXPmo/V/06AAie8ZfZCr3OhLt/uw6QZAoGAdOtBRijHw3F0sSxSN+vs47fmxUSWOnre79aDVIr80x6XTGVNLuQSYvq8WuDLrXEd5LWjUAwaVOxpiDc77OiP79MBws4HNIXOgZ5hUzNvj0N1Uu8uCGujWJbRTEGMRcruoSj1msF8/mAgxXCCdbMKTBPeKsSNIrgpKwNltWOXyAM=", rsa2);
 		String encodedSign = "";
 
 		try {
