e177870a3ef6b308563c27c905bb1409d0ab7108
Dawn <lu923418335@gmail.com>
2018-11-19 01:15:38 +0800

    feature 完善钱包首页页面

--- module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/WalletHomeActivity.java
+++ module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/WalletHomeActivity.java
@@ -1,5 +1,8 @@
 package arouter.dawn.zju.edu.module_wallet;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import baselib.base.BaseActivity;
@@ -7,11 +10,31 @@ import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_WALLET_HOME)
 public class WalletHomeActivity extends BaseActivity<WalletHomeContract.Presenter> implements
-        WalletHomeContract.View {
+        WalletHomeContract.View, View.OnClickListener {
+
+    TextView homeBalanceTv;
 
     @Override
     protected void initView() {
+        homeBalanceTv = findViewById(R.id.wallet_home_balance);
+        findViewById(R.id.wallet_home_forward).setOnClickListener(this);
+        findViewById(R.id.wallet_home_recharge).setOnClickListener(this);
+        findViewById(R.id.wallet_home_bill).setOnClickListener(this);
+        findViewById(R.id.wallet_home_payment_setting).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.wallet_home_forward) {
+            // 充值
+        } else if (id == R.id.wallet_home_recharge) {
+            // 提现
+        } else if (id == R.id.wallet_home_bill) {
+            // 账单
+        } else if (id == R.id.wallet_home_payment_setting) {
+            // 支付设置
+        }
     }
 
     @Override
@@ -23,4 +46,11 @@ public class WalletHomeActivity extends BaseActivity<WalletHomeContract.Presente
     protected void bindPresenter() {
         mPresenter = new WalletHomePresenter();
     }
+
+    @Override
+    protected boolean showHomeAsUp() {
+        return true;
+    }
+
+
 }
