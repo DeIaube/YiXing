6637816445a6c31f8948d7debc4f36b69043d2a7
Dawn <lu923418335@gmail.com>
2018-11-20 15:10:21 +0800

    feature 增加选择支付方式页面

---
+++ module_pay/src/main/java/arouter/dawn/zju/edu/module_pay/ui/select_pay_type/PaySelectPayTypeFragment.java
@@ -0,0 +1,24 @@
+package arouter.dawn.zju.edu.module_pay.ui.select_pay_type;
+
+import android.view.View;
+
+import arouter.dawn.zju.edu.module_pay.R;
+import baselib.base.BaseFragment;
+
+public class PaySelectPayTypeFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_pay_select_pay_type;
+    }
+
+    @Override
+    protected void bindPresenter() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+}
