066d13554f3d474fd9ced9c47dd8e1cc1841cf9a
Dawn <lu923418335@gmail.com>
2018-11-20 15:21:04 +0800

    feature 完善选择支付方式页面

--- module_pay/src/main/java/arouter/dawn/zju/edu/module_pay/ui/select_pay_type/PaySelectPayTypeFragment.java
+++ module_pay/src/main/java/arouter/dawn/zju/edu/module_pay/ui/select_pay_type/PaySelectPayTypeFragment.java
@@ -5,7 +5,7 @@ import android.view.View;
 import arouter.dawn.zju.edu.module_pay.R;
 import baselib.base.BaseFragment;
 
-public class PaySelectPayTypeFragment extends BaseFragment {
+public class PaySelectPayTypeFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     protected int getLayoutId() {
@@ -19,6 +19,20 @@ public class PaySelectPayTypeFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
+        view.findViewById(R.id.pay_type_ali_layout).setOnClickListener(this);
+        view.findViewById(R.id.pay_type_wechat_layout).setOnClickListener(this);
+        view.findViewById(R.id.pay_type_wallet_layout).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.pay_type_ali_layout) {
+            // 选择支付宝支付
+        } else if (id == R.id.pay_type_wechat_layout) {
+            // 选择微信支付
+        } else if (id == R.id.pay_type_wallet_layout) {
+            // 选择余额支付
+        }
     }
 }
