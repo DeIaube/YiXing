4d69b011052e27fe838436761150b33a0eee5450
Dawn <lu923418335@gmail.com>
2018-11-21 17:14:27 +0800

    feature 完善我的银行卡页面

--- module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/bank_card/WalletBankCardActivity.java
+++ module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/bank_card/WalletBankCardActivity.java
@@ -1,5 +1,7 @@
 package arouter.dawn.zju.edu.module_wallet.ui.bank_card;
 
+import android.view.View;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import arouter.dawn.zju.edu.module_wallet.R;
@@ -7,11 +9,11 @@ import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_WALLET_BANK_CARD)
-public class WalletBankCardActivity extends BaseActivity<WalletBankCardContract.Presenter> implements WalletBankCardContract.View {
+public class WalletBankCardActivity extends BaseActivity<WalletBankCardContract.Presenter> implements WalletBankCardContract.View, View.OnClickListener {
 
     @Override
     protected void initView() {
-
+        findViewById(R.id.wallet_bank_card_add_bank_card).setOnClickListener(this);
     }
 
     @Override
@@ -27,5 +29,13 @@ public class WalletBankCardActivity extends BaseActivity<WalletBankCardContract.
     @Override
     protected boolean showHomeAsUp() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.wallet_bank_card_add_bank_card) {
+            
+        }
     }
 }
