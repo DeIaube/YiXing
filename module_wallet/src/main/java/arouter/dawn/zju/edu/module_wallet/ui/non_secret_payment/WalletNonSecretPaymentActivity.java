9de27c8075b4591d0b5c422022a467b0a5c356d6
Dawn <lu923418335@gmail.com>
2018-11-21 19:33:32 +0800

    feature 完善免密支付逻辑

--- module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/non_secret_payment/WalletNonSecretPaymentActivity.java
+++ module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/non_secret_payment/WalletNonSecretPaymentActivity.java
@@ -2,6 +2,9 @@ package arouter.dawn.zju.edu.module_wallet.ui.non_secret_payment;
 
 
 import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 
@@ -10,13 +13,36 @@ import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_WALLET_NON_SECRET_PAYMENT)
-public class WalletNonSecretPaymentActivity extends BaseActivity<WalletNonSecretPaymentContract.Presenter> implements WalletNonSecretPaymentContract.View {
+public class WalletNonSecretPaymentActivity extends BaseActivity<WalletNonSecretPaymentContract.Presenter> implements WalletNonSecretPaymentContract.View, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    private static final int QUOTA_NULL = 0x0;
+    private static final int QUOTA_50 = 0x1;
+    private static final int QUOTA_200 = 0x2;
+    private static final int QUOTA_500 = 0x3;
+    private static final int QUOTA_1000 = 0x4;
 
     SwitchCompat walletNonSecretPaymentSwitch;
+    TextView quota50Tv;
+    TextView quota200Tv;
+    TextView quota500Tv;
+    TextView quota1000Tv;
+
+    private int mCurrentQuota;
 
     @Override
     protected void initView() {
         walletNonSecretPaymentSwitch = findViewById(R.id.wallet_non_secret_payment);
+        quota50Tv = findViewById(R.id.wallet_non_secret_50);
+        quota200Tv = findViewById(R.id.wallet_non_secret_200);
+        quota500Tv = findViewById(R.id.wallet_non_secret_500);
+        quota1000Tv = findViewById(R.id.wallet_non_secret_1000);
+
+        quota50Tv.setOnClickListener(this);
+        quota200Tv.setOnClickListener(this);
+        quota500Tv.setOnClickListener(this);
+        quota1000Tv.setOnClickListener(this);
+        walletNonSecretPaymentSwitch.setOnCheckedChangeListener(this);
+        findViewById(R.id.wallet_non_secret_protocol).setOnClickListener(this);
     }
 
     @Override
@@ -32,5 +58,67 @@ public class WalletNonSecretPaymentActivity extends BaseActivity<WalletNonSecret
     @Override
     protected boolean showHomeAsUp() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.wallet_non_secret_protocol) {
+            // 跳转到免密协议
+        } else if (id == R.id.wallet_non_secret_50) {
+            mCurrentQuota = QUOTA_50;
+            resetQuota();
+            walletNonSecretPaymentSwitch.setChecked(true);
+        } else if (id == R.id.wallet_non_secret_200) {
+            mCurrentQuota = QUOTA_200;
+            resetQuota();
+            walletNonSecretPaymentSwitch.setChecked(true);
+        } else if (id == R.id.wallet_non_secret_500) {
+            mCurrentQuota = QUOTA_500;
+            resetQuota();
+            walletNonSecretPaymentSwitch.setChecked(true);
+        } else if (id == R.id.wallet_non_secret_1000) {
+            mCurrentQuota = QUOTA_1000;
+            resetQuota();
+            walletNonSecretPaymentSwitch.setChecked(true);
+        }
+    }
+
+    private void resetQuota() {
+        quota50Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_unselect_bg);
+        quota200Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_unselect_bg);
+        quota500Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_unselect_bg);
+        quota1000Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_unselect_bg);
+
+        quota50Tv.setTextColor(getResources().getColor(R.color.bec2c9));
+        quota200Tv.setTextColor(getResources().getColor(R.color.bec2c9));
+        quota500Tv.setTextColor(getResources().getColor(R.color.bec2c9));
+        quota1000Tv.setTextColor(getResources().getColor(R.color.bec2c9));
+
+        if (mCurrentQuota == QUOTA_50) {
+            quota50Tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            quota50Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_select_bg);
+        } else if (mCurrentQuota == QUOTA_200) {
+            quota200Tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            quota200Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_select_bg);
+        } else if (mCurrentQuota == QUOTA_500) {
+            quota500Tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            quota500Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_select_bg);
+        } else if (mCurrentQuota == QUOTA_1000) {
+            quota1000Tv.setTextColor(getResources().getColor(R.color.colorPrimary));
+            quota1000Tv.setBackgroundResource(R.drawable.wallet_non_secret_payment_quota_select_bg);
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            if (mCurrentQuota == QUOTA_NULL) {
+                mCurrentQuota = QUOTA_50;
+            }
+        } else {
+            mCurrentQuota = QUOTA_NULL;
+        }
+        resetQuota();
     }
 }
