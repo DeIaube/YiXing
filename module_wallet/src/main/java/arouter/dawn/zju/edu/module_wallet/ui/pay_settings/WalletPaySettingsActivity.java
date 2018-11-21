7331be2f3eb80eeda4e77fd93e56b6847d388428
Dawn <lu923418335@gmail.com>
2018-11-21 18:21:31 +0800

    feature 完善部分支付设置逻辑

--- module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/pay_settings/WalletPaySettingsActivity.java
+++ module_wallet/src/main/java/arouter/dawn/zju/edu/module_wallet/ui/pay_settings/WalletPaySettingsActivity.java
@@ -1,5 +1,10 @@
 package arouter.dawn.zju.edu.module_wallet.ui.pay_settings;
 
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import arouter.dawn.zju.edu.module_wallet.R;
@@ -7,11 +12,20 @@ import baselib.base.BaseActivity;
 import baselib.config.Constants;
 
 @Route(path = Constants.AROUTER_WALLET_PAY_SETTINGS)
-public class WalletPaySettingsActivity extends BaseActivity<WalletPaySettingsContract.Presenter> implements WalletPaySettingsContract.View {
+public class WalletPaySettingsActivity extends BaseActivity<WalletPaySettingsContract.Presenter> implements WalletPaySettingsContract.View, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+
+    Switch walletSettingsFingerprintPaymentSwitch;
+    TextView walletSettingsNonSecretPaymentState;
 
     @Override
     protected void initView() {
+        walletSettingsFingerprintPaymentSwitch = findViewById(R.id.wallet_settings_fingerprint_payment);
+        walletSettingsNonSecretPaymentState = findViewById(R.id.wallet_settings_non_secret_payment_state);
 
+        findViewById(R.id.wallet_settings_alert_pay_password).setOnClickListener(this);
+        findViewById(R.id.wallet_settings_reset_pay_password).setOnClickListener(this);
+        findViewById(R.id.wallet_settings_non_secret_payment).setOnClickListener(this);
+        walletSettingsFingerprintPaymentSwitch.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -27,5 +41,22 @@ public class WalletPaySettingsActivity extends BaseActivity<WalletPaySettingsCon
     @Override
     protected boolean showHomeAsUp() {
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.wallet_settings_alert_pay_password) {
+            // 修改支付密码
+        } else if (id == R.id.wallet_settings_reset_pay_password) {
+            // 忘记(重置)支付密码
+        } else if (id == R.id.wallet_settings_non_secret_payment) {
+            // 小额免密
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
     }
 }
