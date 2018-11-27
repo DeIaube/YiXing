06a0e2b7e64c6cd0747019dac7776f3cb1826cdb
Dawn <lu923418335@gmail.com>
2018-11-27 16:26:25 +0800

    feature 完善查看用户信息页面

--- module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ui/user_information/ForumUserInformationActivity.java
+++ module_forum/src/main/java/arouter/dawn/zju/edu/module_forum/ui/user_information/ForumUserInformationActivity.java
@@ -1,5 +1,11 @@
 package arouter.dawn.zju.edu.module_forum.ui.user_information;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 
 import arouter.dawn.zju.edu.module_forum.R;
@@ -9,9 +15,25 @@ import baselib.config.Constants;
 @Route(path = Constants.AROUTER_FORUM_USER_INFORMATION)
 public class ForumUserInformationActivity extends BaseActivity<ForumUserInformationContract.Presenter> implements ForumUserInformationContract.View {
 
+    ImageView userPortraitIv;
+    TextView userPicknameTv;
+    Button userFollowBtn;
+    TextView userFollowCountTv;
+    TextView userFollowedCountTv;
+    TextView userPostCountTv;
+    RecyclerView userPostListRv;
+
     @Override
     protected void initView() {
+        userPortraitIv = findViewById(R.id.forum_user_information_portrait);
+        userPicknameTv = findViewById(R.id.forum_user_information_pickname);
+        userFollowBtn = findViewById(R.id.forum_user_information_follow);
+        userFollowCountTv = findViewById(R.id.forum_user_information_follow_count);
+        userFollowedCountTv = findViewById(R.id.forum_user_information_followed_count);
+        userPostCountTv = findViewById(R.id.forum_user_information_post_count);
+        userPostListRv = findViewById(R.id.forum_user_information_post_list);
 
+        userPostListRv.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
